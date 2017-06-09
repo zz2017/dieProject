134bcf42d2836e9f190071861d74ff926ed8e44e
xiao xiao <xiao xiao@xiaoxiao-PC>
2017-06-09 15:34:13 +0800

    show()

--- gitTest/src/tset/Ta.java
+++ gitTest/src/tset/Ta.java
@@ -4,8 +4,11 @@ public class Ta {
 	public static void print(){
 		
 	}
+	public int sum(int x,int y){
+		return x+y;
+	}
+	public static void show(){}
 	public static void main(String[] args) {
-		Ta ata = new Ta();
-		ata.print();
+		
 	}
 }
